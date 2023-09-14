bbcf0db4f7a8df24e65ef52a08bc4a3766b6c63a
guskumite <gmolanoruiz2000@gmail.com>
2023-09-14 00:37:21 -0500

    version 1.0.23 Completion of Hard and Soft Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,29 +15,42 @@ const Skills = () => {
   const [Labels, setLabels] = useState(0);
   const [Dark, setDark] = useState(0);
   const [Left1, setLeft1] = useState("left-[15rem]");
+  const [Left2, setLeft2] = useState("left-[29rem]");
+  const [Label2, setLabel2] = useState(["Soft Skills", "Habilidades Blandas"]);
+  const [Label3, setLabel3] = useState([
+    "Learning constantly",
+    "Aprendizaje constante",
+  ]);
+  const [Label4, setLabel4] = useState([
+    "Take challenges in a creative way to find the best solution",
+    "  Asumir retos de forma creativa para llegar a la mejor solución",
+  ]);
+  const [Label5, setLabel5] = useState(["Team work", "Trabajo en equipo"]);
+  const [Label6, setLabel6] = useState(["Resiliency", "Resiliencia"]);
+  const [Label7, setLabel7] = useState([
+    "Assertive communication",
+    "Comunicación Asertiva",
+  ]);
+  const [Label8, setLabel8] = useState([
+    "Persist in achieving objectives",
+    "Persistencia en el logro de objetivos",
+  ]);
+  const [Label9, setLabel9] = useState([
+    "Analytic Thinking",
+    "Pensamiento analítico",
+  ]);
+
+  const [LabelA, setLabelA] = useState(["Self learning", "Autoaprendizaje"]);
+  const [LabelB, setLabelB] = useState(["Self management", "Autogestión"]);
 
   function toggleOpen() {
     setIsOpen(!isOpen);
     localStorage.setItem("navbarOpen", isOpen);
   }
 
+  const Selector1 = ["Soft Skills", "Habilidades Blandas"];
+  const Selector2 = ["Hard Skills", "Habilidades Duras"];
   const label1 = ["Skills", "Habilidades"];
-  const label2 = ["Soft Skills", "Habilidades Blandas"];
-  const label3 = ["Learning constantly", "Aprendizaje constante"];
-  const label4 = [
-    "Take challenges in a creative way to find the best solution",
-    "  Asumir retos de forma creativa para llegar a la mejor solución",
-  ];
-  const label5 = ["Team work", "Trabajo en equipo"];
-  const label6 = ["Resiliency", "Resiliencia"];
-  const label7 = ["Assertive communication", "Comunicación Asertiva"];
-  const label8 = [
-    "Persist in achieving objectives",
-    "Persistencia en el logro de objetivos",
-  ];
-  const label9 = ["Analytic Thinking", "Pensamiento analítico"];
-  const labelA = ["Self learning", "Autoaprendizaje"];
-  const labelB = ["Self management", "Autogestión"];
 
   const Home = ["Home", "Inicio"];
   const About = ["About", "Acerca de mi"];
@@ -49,9 +62,11 @@ const Skills = () => {
     if (window.innerWidth < 600) {
       setBgImg(mobileImg);
       setLeft1("left-[2rem]");
+      setLeft2("left-[15rem]");
     } else {
       setBgImg(desktopImg);
       setLeft1("left-[15rem]");
+      setLeft2("left-[29rem]");
     }
   };
 
@@ -113,6 +128,63 @@ const Skills = () => {
     }
   };
 
+  const handleSkills = (e) => {
+    if (e.target.name === "soft") {
+      setLabel2(["Soft Skills", "Habilidades Blandas"]);
+      setLabel3(["Learning constantly", "Aprendizaje constante"]);
+      setLabel4([
+        "Take challenges in a creative way to find the best solution",
+        "  Asumir retos de forma creativa para llegar a la mejor solución",
+      ]);
+      setLabel5(["Team work", "Trabajo en equipo"]);
+      setLabel6(["Resiliency", "Resiliencia"]);
+      setLabel7(["Assertive communication", "Comunicación Asertiva"]);
+      setLabel8([
+        "Persist in achieving objectives",
+        "Persistencia en el logro de objetivos",
+      ]);
+      setLabel9(["Analytic Thinking", "Pensamiento analítico"]);
+      setLabelA(["Self learning", "Autoaprendizaje"]);
+      setLabelB(["Self management", "Autogestión"]);
+    }
+    if (e.target.name === "hard") {
+      setLabel2(["Hard Skills", "Habilidades Duras"]);
+      setLabel3([
+        "Experience in Software Technical Support",
+        "Experiencia en Soporte Técnico de Software",
+      ]);
+      setLabel4([
+        "Knowledge of Oracle SQL databases and other SQL databases",
+        "Conocimiento de bases de datos de Oracle y de otras bases de datos SQL",
+      ]);
+      setLabel5([
+        "Academlo: Recent Certified Knowledge of FrontEnd and BackEnd Development",
+        "Academlo: Conocimiento Reciente certificado en desarrollo FrontEnd y BackEnd",
+      ]);
+      setLabel6([
+        "Cisco: Introduction to Cybersecurity and Cybersecurity Essentials",
+        "Cisco: Introducción a la Ciberseguridad y Esenciales de Ciberseguridad",
+      ]);
+      setLabel7(["Intermediate English", "Inglés Intermedio"]);
+      setLabel8([
+        "Basic Knowledge of prompts for generative AI like ChatGPT, Claude, DALL-E, etc.",
+        "Conocimiento básico de prompts para IA generativa como ChatGPT, Claude, DALL-E, etc. ",
+      ]);
+      setLabel9([
+        "Knowledge of Windows, Linux, Ubuntu",
+        "Conocimiento de Windows, Linux, Ubuntu",
+      ]);
+      setLabelA([
+        "Knowledge of ofimatic suites like Office and OpenOffice",
+        "Conocimiento de herramientas ofimáticas como Office y OpenOffice",
+      ]);
+      setLabelB([
+        "Experience in code maintainance in different platforms and languages",
+        "Experiencia en mantenimiento de código en diversas plataformas y lenguajes",
+      ]);
+    }
+  };
+
   return (
     <main className="absolute top-[0rem] bg-cover h-screen w-screen text-gray-400">
       <img className="w-full min-w-[320px] opacity-80" src={bgImg} />
@@ -131,24 +203,44 @@ const Skills = () => {
         >
           Darkmode | Modo oscuro 🌞🌛
         </button>
+        <button
+          className={`absolute top-[9.6rem] ${Left1} text-zinc-300 text-md
+         border-[#D2D8DA] border-2 bg-[#230B06] hover:text-xl`}
+          onClick={(e) => {
+            handleSkills(e);
+          }}
+          name="soft"
+        >
+          {Selector1[Labels]}
+        </button>
+        <button
+          className={`absolute top-[9.6rem] ${Left2} text-zinc-300 text-md
+         border-[#D2D8DA] border-2 bg-[#230B06] hover:text-xl`}
+          onClick={(e) => {
+            handleSkills(e);
+          }}
+          name="hard"
+        >
+          {Selector2[Labels]}
+        </button>
       </div>
       <div
         className={`w-[50%] absolute top-[12rem] bg-zinc-300 text-[#333] ${Left1}`}
       >
         <h1 className="font-bold text-lg">{label1[Labels]}</h1>
         <br></br>
-        <p> {label2[Labels]} </p>
+        <p> {Label2[Labels]} </p>
         <br></br>
         <ul className="">
-          <li className="border-2 border-red-300">{label3[Labels]}</li>
-          <li className="border-2 border-red-300">{label4[Labels]}</li>
-          <li className="border-2 border-red-300">{label5[Labels]}</li>
-          <li className="border-2 border-red-300">{label6[Labels]}</li>
-          <li className="border-2 border-red-300">{label7[Labels]}</li>
-          <li className="border-2 border-red-300">{label8[Labels]}</li>
-          <li className="border-2 border-red-300">{label9[Labels]}</li>
-          <li className="border-2 border-red-300">{labelA[Labels]}</li>
-          <li className="border-2 border-red-300">{labelB[Labels]}</li>
+          <li className="border-2 border-red-300">{Label3[Labels]}</li>
+          <li className="border-2 border-red-300">{Label4[Labels]}</li>
+          <li className="border-2 border-red-300">{Label5[Labels]}</li>
+          <li className="border-2 border-red-300">{Label6[Labels]}</li>
+          <li className="border-2 border-red-300">{Label7[Labels]}</li>
+          <li className="border-2 border-red-300">{Label8[Labels]}</li>
+          <li className="border-2 border-red-300">{Label9[Labels]}</li>
+          <li className="border-2 border-red-300">{LabelA[Labels]}</li>
+          <li className="border-2 border-red-300">{LabelB[Labels]}</li>
         </ul>
       </div>
     </main>
